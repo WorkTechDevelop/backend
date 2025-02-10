d563f69f51d8ac456d6c816548e796c2f5e610ba
Deffris <nesevodny5@gmail.ru>
2025-02-10 14:46:40 +0300

    -update SecurityConfig(set anyRequest().authenticated()) -fix RegisterDto 1.add @Data, delete setter/getter 2.add message for middleName

--- src/main/java/ru/worktechlab/work_task/model/rest/RegisterDto.java
+++ src/main/java/ru/worktechlab/work_task/model/rest/RegisterDto.java
@@ -1,5 +1,7 @@
 package ru.worktechlab.work_task.model.rest;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import ru.worktechlab.work_task.model.db.enums.Gender;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
@@ -7,8 +9,7 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
-
-
+@Data
 public class RegisterDto {
 
     @NotBlank(message = "Email не может быть пустым")
@@ -26,8 +27,11 @@ public class RegisterDto {
 
     @NotBlank(message = "Фамилия не может быть пустым")
     @NotEmpty
-    private String middleName;
     private String lastName;
+
+    @NotBlank(message = "Отчество не может быть пустым")
+    @NotEmpty
+    private String middleName;
 
     private String phone;
 
@@ -36,76 +40,4 @@ public class RegisterDto {
     //@ValidGender
     @Enumerated(EnumType.STRING)
     private Gender gender;
-
-    public String getEmail() {
-        return this.email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getConfirmPassword() {
-        return confirmPassword;
-    }
-
-    public void setConfirmPassword(String confirmPassword) {
-        this.confirmPassword = confirmPassword;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getMiddleName() {
-        return middleName;
-    }
-
-    public void setMiddleName(String middleName) {
-        this.middleName = middleName;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public String getBirthDate() {
-        return birthDate;
-    }
-
-    public void setBirthDate(String birthDate) {
-        this.birthDate = birthDate;
-    }
-
-    public Gender getGender() {
-        return gender;
-    }
-
-    public void setGender(Gender gender) {
-        this.gender = gender;
-    }
 }
