6ed797305d092559af5c7ae812207718c694622d
Segasegasson <sergeyeremenkodeployer@gmail.com>
2025-03-15 12:55:28 +0300

    delete import

--- src/main/java/ru/worktechlab/work_task/model/rest/RegisterDto.java
+++ src/main/java/ru/worktechlab/work_task/model/rest/RegisterDto.java
@@ -4,7 +4,6 @@ import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 import ru.worktechlab.work_task.model.db.enums.Gender;
