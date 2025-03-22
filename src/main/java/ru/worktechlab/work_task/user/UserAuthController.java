029625875ab645693a4a9935ecc1253addccdcbc
Segasegasson <sergeyeremenkodeployer@gmail.com>
2025-03-22 10:41:31 +0300

    deleted don't used fields

--- src/main/java/ru/worktechlab/work_task/user/UserAuthController.java
+++ src/main/java/ru/worktechlab/work_task/user/UserAuthController.java
@@ -1,11 +1,8 @@
 package ru.worktechlab.work_task.user;
 
-import ru.worktechlab.work_task.jwt.JwtUtils;
 import ru.worktechlab.work_task.model.rest.LoginRequest;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.web.bind.annotation.*;
 import ru.worktechlab.work_task.user.service.AuthService;
 
@@ -15,12 +12,6 @@ import ru.worktechlab.work_task.user.service.AuthService;
 public class UserAuthController {
 
     private final AuthService authService;
-
-    @Autowired
-    private AuthenticationManager authenticationManager;
-
-    @Autowired
-    private JwtUtils jwtUtils;
 
     @GetMapping("/welcome")
     public String welcome() {
