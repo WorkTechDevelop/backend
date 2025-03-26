f4d72bdb960d37283b2a3f3262c06ad65f73fc2b
Segasegasson <sergeyeremenkodeployer@gmail.com>
2025-03-26 19:04:53 +0300

    add TokenService

--- src/main/java/ru/worktechlab/work_task/user/service/AuthService.java
+++ src/main/java/ru/worktechlab/work_task/user/service/AuthService.java
@@ -6,7 +6,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import ru.worktechlab.work_task.config.CustomUserDetails;
-import ru.worktechlab.work_task.jwt.JwtUtils;
+import ru.worktechlab.work_task.jwt.TokenService;
 import ru.worktechlab.work_task.model.mappers.AuthMapper;
 import ru.worktechlab.work_task.model.rest.LoginRequest;
 import ru.worktechlab.work_task.model.rest.LoginResponse;
@@ -16,8 +16,8 @@ import ru.worktechlab.work_task.model.rest.LoginResponse;
 public class AuthService {
 
     private final AuthenticationManager authenticationManager;
-    private final JwtUtils jwtUtils;
     private final AuthMapper authMapper;
+    private final TokenService tokenService;
 
     public LoginResponse authenticate(LoginRequest loginRequest) {
         Authentication authentication = authenticationManager.authenticate(
@@ -26,7 +26,7 @@ public class AuthService {
         SecurityContextHolder.getContext().setAuthentication(authentication);
 
         CustomUserDetails userDetails = (CustomUserDetails) authentication.getPrincipal();
-        String jwtToken = jwtUtils.generateTokenFromUserDetails(userDetails);
+        String jwtToken = tokenService.generateToken(userDetails);
         return authMapper.toLoginResponse(userDetails, jwtToken);
     }
 }
