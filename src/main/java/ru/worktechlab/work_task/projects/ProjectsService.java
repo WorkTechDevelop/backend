52d244e7a5130a3915f14553caf187f4b38093f1
Segasegasson <sergeyeremenkodeployer@gmail.com>
2025-03-26 19:53:04 +0300

    update ProjectsService, ProjectsController

--- src/main/java/ru/worktechlab/work_task/projects/ProjectsService.java
+++ src/main/java/ru/worktechlab/work_task/projects/ProjectsService.java
@@ -4,9 +4,11 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import ru.worktechlab.work_task.jwt.JwtUtils;
 import ru.worktechlab.work_task.model.db.TaskModel;
+import ru.worktechlab.work_task.responseDTO.UsersProjectsDTO;
 import ru.worktechlab.work_task.task.TaskService;
 import ru.worktechlab.work_task.user.UserRepository;
 
+import java.util.Collections;
 import java.util.List;
 
 @Service
@@ -16,10 +18,16 @@ public class ProjectsService {
     private UserRepository userRepository;
     private UsersProjectsRepository usersProjectsRepository;
     private TaskService taskService;
+    private ProjectRepository projectRepository;
 
-
-    public List<String> getAllUserProjects(String jwtToken) {
-        return usersProjectsRepository.findProjectsByUserId(jwtUtils.getUserGuidFromJwtToken(jwtToken));
+    public List<UsersProjectsDTO> getAllUserProjects(String jwtToken) {
+        List<String> projectIds = usersProjectsRepository.findProjectsByUserId(
+                jwtUtils.getUserGuidFromJwtToken(jwtToken)
+        );
+        if (projectIds.isEmpty()) {
+            return Collections.emptyList();
+        }
+        return projectRepository.findProjectIdAndNameByIds(projectIds);
     }
 
     public List<TaskModel> setMainProject(String projectId, String jwtToken) {
