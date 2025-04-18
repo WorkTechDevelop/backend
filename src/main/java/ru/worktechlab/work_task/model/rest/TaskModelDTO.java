6f8fdb19df30442913cc8db98ff55b2271d28f1e
Segasegasson <sergeyeremenkodeployer@gmail.com>
2025-04-18 17:00:05 +0300

    delete import

--- src/main/java/ru/worktechlab/work_task/model/rest/TaskModelDTO.java
+++ src/main/java/ru/worktechlab/work_task/model/rest/TaskModelDTO.java
@@ -1,7 +1,9 @@
 package ru.worktechlab.work_task.model.rest;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-import jakarta.validation.constraints.*;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 import ru.worktechlab.work_task.task.validators.*;
 
