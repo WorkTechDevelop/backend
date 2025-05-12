422162e0546052e30612ff4a3e59a5a7621d46d1
Segasegasson <sergeyeremenkodeployer@gmail.com>
2025-05-12 20:15:24 +0300

    Fix validation UpdateTaskModelDTO

---
+++ src/main/java/ru/worktechlab/work_task/task/validators/ValidDescription.java
@@ -0,0 +1,18 @@
+package ru.worktechlab.work_task.task.validators;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.*;
+
+@Target({ElementType.FIELD, ElementType.PARAMETER})
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = DesriptionValidator.class)
+@Documented
+public @interface ValidDescription {
+    String message() default "Длина поля DESCRIPTION не может быть более 4096 символов";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+}
