c4feea4f32a48d7b28bf823a52a559ef3bc1214a
Еременко Сергей Александрович <s.a.eremenko@tinkoff.ru>
2025-05-23 12:09:10 +0300

    add UserContext and set AuthTokenFilter draft

--- src/main/java/ru/worktechlab/work_task/utils/UserContext.java
+++ src/main/java/ru/worktechlab/work_task/utils/UserContext.java
@@ -1,16 +1,41 @@
 package ru.worktechlab.work_task.utils;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import org.springframework.stereotype.Component;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-@Builder
+@Component
+@RequiredArgsConstructor
 public class UserContext {
-    private Long userId;
-    private String email;
-    private String role;
+    private final ThreadLocal<UserContextData> threadLocal = new ThreadLocal<>();
+
+    // TODO Добавить ошибку
+    // паттерн визитер чекнуть
+    public UserContextData getUserData() throws Exception {
+        UserContextData userContextData = threadLocal.get();
+        if (userContextData == null) {
+            throw new Exception("ERROR");
+        }
+        return userContextData;
+    }
+
+    public void setThreadLocal(String userId, String email, String roleId) {
+        threadLocal.set(new UserContextData(userId, email, roleId));
+    }
+
+    public void clearThreadLocal() {
+        threadLocal.remove();
+    }
+
+    @Data
+    public class UserContextData {
+        private String userId;
+        private String email;
+        private String roleId;
+
+        public UserContextData(String userId, String email, String roleId) {
+            this.userId = userId;
+            this.email = email;
+            this.roleId = roleId;
+        }
+    }
 }
