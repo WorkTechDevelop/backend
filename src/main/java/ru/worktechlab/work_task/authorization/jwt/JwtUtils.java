a29249f3744528effcf7bb85c3a4aa4a15370f77
Еременко Сергей Александрович <s.a.eremenko@tinkoff.ru>
2025-05-26 18:13:28 +0300

    delete import

--- src/main/java/ru/worktechlab/work_task/authorization/jwt/JwtUtils.java
+++ src/main/java/ru/worktechlab/work_task/authorization/jwt/JwtUtils.java
@@ -10,7 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
-import ru.worktechlab.work_task.config.CustomUserDetails;
 import ru.worktechlab.work_task.models.tables.RefreshToken;
 import ru.worktechlab.work_task.models.tables.User;
 import ru.worktechlab.work_task.repositories.RefreshTokenRepository;
